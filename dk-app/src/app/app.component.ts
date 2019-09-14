310efca7ed97ff47c893a53b973d492292c2f33c
James Dougherty <jjdougherty89@gmail.com>
2019-09-14 18:56:25 -0400

    Create DK landing page

--- dk-app/src/app/app.component.ts
+++ dk-app/src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title: string;
 
   constructor() {
-    this.title = 'Dawn Koehler Insurance Agency, LLC';
+    this.title = 'Dawn Koehler';
   }
 }
--- dk-app/src/app/app.module.ts
+++ dk-app/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
