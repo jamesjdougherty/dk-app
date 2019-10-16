09a8ccf27c97c1d588711efa66ef379a48aa4530
James Dougherty <jjdougherty89@gmail.com>
2019-10-15 20:41:55 -0400

    Add new components for MPA

--- dk-app/src/app/app.module.ts
+++ dk-app/src/app/app.module.ts
@@ -3,22 +3,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
-
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServicesComponent,
+    QuotesComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
